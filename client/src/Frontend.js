5f4d5b76d08a4b57f53451a6137fbd63c205d377
Alex <alexd@pareto.local>
2018-03-15 18:41:40 +0200

    ActiveRunning component

--- client/src/Frontend.js
+++ client/src/Frontend.js
@@ -1,22 +1,192 @@
 import React, { Component } from 'react';
 import './Frontend.css';
+import { isNumber, isNull } from 'util';
+
+function SecondsToTimeFormat(seconds) {
+  if (isNull(seconds) || !isNumber(seconds))
+    return "";
+
+  var sMinutes = parseInt(seconds / 60, 10).toString();
+  while (sMinutes.length < 2) {
+    sMinutes = '0' + sMinutes;
+  }
+
+  var sSeconds = parseInt(seconds % 60, 10).toString();
+  while (sSeconds.length < 2) {
+    sSeconds = '0' + sSeconds;
+  }
+
+  return sMinutes + ":" + sSeconds;
+}
+
+class ListOfRaces extends Component {
+  render () {
+
+    const tableContent = this.props.races.map((element) =>
+      <tr>
+        <td>{SecondsToTimeFormat(element.start)}</td>
+        <td>{SecondsToTimeFormat(element.stop)}</td>
+        <td>{SecondsToTimeFormat(element.time)}</td>
+        <td>{element.speed}</td>
+        <td>{element.distance}</td>
+      </tr>
+    );
+
+    return (
+      <table className="RacesTable">
+        <thead>
+          <tr>
+            <th>Start</th>
+            <th>Stop</th>
+            <th>Time</th>
+            <th>Speed</th>
+            <th>Distance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableContent}
+          <tr>
+              <td></td>
+              <td></td>
+              <td>{SecondsToTimeFormat(this.props.runningTime)}</td>
+              <td></td>
+              <td>{this.props.runningDistance}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
 
 class ActiveTraining extends Component {
   constructor (props) {
     super(props);
 
     this.state = {
-      status: "walking"
+      runningStatus: "walking",
+      time: 0
     };
+
+    this.startRunning = this.startRunning.bind(this);
+    this.stopRunning = this.stopRunning.bind(this);
+  }
+
+  listOfRacesComponent (races, runningTime, runningDistance) {
+    return <ListOfRaces races={races} runningTime={runningTime} runningDistance={runningDistance} />
+  }
+
+  startRunning (e) {
+    e.preventDefault();
+
+    let startRaceTime = 0;
+    while (startRaceTime < this.props.trainingTime + 15) {
+      startRaceTime += 30;
+    }
+    let timeAhead = startRaceTime - this.props.trainingTime;
+
+    this.setState({
+      runningStatus: "starting",
+      start: startRaceTime,
+      time: timeAhead
+    });
+
+    this.timeID = setInterval(
+      () => this.raceTick(),
+      1000
+    );
+  }
+
+  stopRunning (e) {
+    e.preventDefault();
+
+    clearInterval(this.timeID);
+
+    // if race was actually started then store the race, otherwise cancel it
+    if (this.state.runningStatus === "running") {
+
+        var stoptime = this.state.start + this.state.time;
+        var distance = parseInt(8000 / 3600 * this.state.time);
+
+        this.props.addRace({
+          start: this.state.start,
+          stop: stoptime,
+          time: this.state.time,
+          speed: 8.0,
+          distance: distance
+        });
+    }
+
+    this.setState({
+      runningStatus: "walking",
+      time: 0
+    });
+  }
+
+  raceTick () {
+    if (this.state.runningStatus === "starting") {
+      this.setState((prevState, props) => ({
+        time: prevState.time - 1
+      }));
+    }
+    else {
+      this.setState((prevState, props) => ({
+        time: prevState.time + 1
+      }));
+    }
+
+    if (this.state.time == 0) {
+      this.setState({
+        runningStatus: "running"
+      });
+    }
+  }
+
+  raceCtrlBlock () {
+    switch (this.state.runningStatus) {
+      case "walking":
+        return (
+          <div className="RaceCtrlBlockHolder">
+            <div className="LargeCtrlBtnHolder">
+              <div className="RaceTime RaceTimeWalking">{SecondsToTimeFormat(this.state.time)}</div><br />
+              <a href="./" className="MainCtrlButton GreenCtrlButton LargeCtrlButton" onClick={this.startRunning}>RUN</a>
+            </div>
+          </div>
+        );
+
+      case "starting":
+        return (
+          <div className="RaceCtrlBlockHolder">
+            <div className="LargeCtrlBtnHolder">
+              <div className="RaceTime RaceTimeStarting">{SecondsToTimeFormat(this.state.time)}</div><br />
+              <a href="./" className="MainCtrlButton GreenCtrlButton LargeCtrlButton" onClick={this.stopRunning}>STOP</a>
+            </div>
+          </div>          
+        );
+
+      case "running":
+        return (
+          <div className="RaceCtrlBlockHolder">
+            <div className="LargeCtrlBtnHolder">
+              <div className="RaceTime RaceTimeRunning">{SecondsToTimeFormat(this.state.time)}</div><br />
+              <a href="./" className="MainCtrlButton GreenCtrlButton LargeCtrlButton" onClick={this.stopRunning}>STOP</a>
+            </div>
+          </div>
+        );
+
+      default:
+        break;
+    }
   }
 
   render () {
 
+    const ListOfRacesComponent = this.listOfRacesComponent(this.props.races, this.props.runningTime, this.props.runningDistance);
+    const RaceCtrlBlock = this.raceCtrlBlock();
+
     return (
       <div className="ActiveTraining">
-        ACTIVE TRAINING
-        <div>Status is {this.props.status}</div>
-        <div>Training time {this.props.trainingTime} seconds</div>
+        {ListOfRacesComponent}
+        {RaceCtrlBlock}
       </div>
     );
   }
@@ -35,13 +205,13 @@ class Frontend extends Component {
     super(props);
 
     this.state = {
-      status: "initial",
-      trainingTime: 0
+      status: "initial"
     };
 
     this.startTraining = this.startTraining.bind(this);
     this.finishTraining = this.finishTraining.bind(this);
     this.storeTraining = this.storeTraining.bind(this);
+    this.addRace = this.addRace.bind(this);
   }
 
   startTraining (e) {
@@ -50,11 +220,28 @@ class Frontend extends Component {
     this.setState({
       status: "training",
       trainingTime: 0,
-      races: []
+      races: [
+        {
+          start: 120,
+          stop: 300,
+          time: 180,
+          speed: 8.0,
+          distance: 400
+        },
+        {
+          start: 480,
+          stop: 660,
+          time: 180,
+          speed: 8.0,
+          distance: 400
+        }
+      ],
+      runningTime: 360,
+      runningDistance: 800
     });
 
     this.timeID = setInterval(
-      () => this.tick(),
+      () => this.trainingTick(),
       1000
     );
   }
@@ -68,7 +255,7 @@ class Frontend extends Component {
     });
   }
 
-  tick () {
+  trainingTick () {
     this.setState((prevState, props) => ({
       trainingTime: prevState.trainingTime + 1
     }));
@@ -111,11 +298,24 @@ class Frontend extends Component {
     }
   }
 
-  trainingCoponent (status, trainingTime) {
+  addRace(raceData) {
+    var updatedRaces = this.state.races.slice();
+    updatedRaces.push(raceData);
+    var updatedRunningTime = this.state.runningTime + raceData.time;
+    var updatedRunningDistance = this.state.runningDistance + raceData.distance;
+    this.setState({
+      races: updatedRaces,
+      runningTime: updatedRunningTime,
+      runningDistance: updatedRunningDistance
+    });
+  }
+
+  trainingComponent (status, trainingTime, races, runningTime, runningDistance, addRace) {
     switch (this.state.status)
     {
       case "training":
-        return <ActiveTraining status={status} trainingTime={trainingTime} />
+        return <ActiveTraining status={status} trainingTime={trainingTime} races={races} 
+          runningTime={runningTime} runningDistance={runningDistance} addRace={addRace} />
 
       case "pending_store":
         return <FinishedTraining />
@@ -127,12 +327,13 @@ class Frontend extends Component {
 
   render () {
 
-    const TrainingCoponent = this.trainingCoponent(this.state.status, this.state.trainingTime);
+    const trainingComponent = this.trainingComponent(this.state.status, this.state.trainingTime, this.state.races,
+      this.state.runningTime, this.state.runningDistance, this.addRace);
     const TrainingButton = this.trainingButton();
 
     return (
         <div className="Frontend">
-            {TrainingCoponent}
+            {trainingComponent}
             {TrainingButton}
         </div>
     );
