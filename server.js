098ccffa081c94f481541050250c7ef59327d062
Alex <alexd@pareto.local>
2018-03-29 17:18:44 +0300

    require path added to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var trainingModel = require('./models/training.js');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
